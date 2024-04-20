9f2829c22328640b0476fd8179e0fef90c18cb17
许大仙 <1900919313@qq.com>
2024-04-20 22:28:01 +0800

    修改配置

--- src/test/java/com/github/mysql/dev/LocalDevTestApplication.java
+++ src/test/java/com/github/mysql/dev/LocalDevTestApplication.java
@@ -1,32 +1,16 @@
 package com.github.mysql.dev;
 
 import com.github.Application;
-import com.github.dao.CustomerRepository;
-import com.github.domain.Customer;
-import jakarta.annotation.Resource;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.List;
-
-@Slf4j
-@Testcontainers
 public class LocalDevTestApplication {
 
-    @Resource
-    private CustomerRepository customerRepository;
 
     public static void main(String[] args) {
         SpringApplication
                 .from(Application::main)
-                .with(LocalDevTestContainersConfiguration.class)
+                .with(LocalDevTestcontainersConfig.class)
                 .run(args);
-    }
-
-    public void test() {
-        List<Customer> all = customerRepository.findAll();
-        log.info("LocalDevTestApplication.test ==> {}", all);
     }
 
 }
--- src/test/java/com/github/mysql/dev/LocalDevTestcontainersConfig.java
+++ src/test/java/com/github/mysql/dev/LocalDevTestcontainersConfig.java
@@ -5,14 +5,15 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class LocalDevTestContainersConfiguration {
-
+public class LocalDevTestcontainersConfig {
     @Bean
     @RestartScope
     @ServiceConnection
-    public MySQLContainer<?> mysql() {
-        return new MySQLContainer<>("mysql:8");
+    public MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("mysql:8"));
     }
+
 }
